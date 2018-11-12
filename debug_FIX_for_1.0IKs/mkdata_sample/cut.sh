35a5edc22c3c0228c6219b26c9db64b7644c158a
Kunichika Tsumoto <42426029+92tsumoto@users.noreply.github.com>
2018-11-12 19:12:44 +0900

    Add files via upload

---
+++ debug_FIX_for_1.0IKs/mkdata_sample/cut.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# PL-gnu.sh
+
+cat data.out | awk '{print $2,$3}' > ap.dat
+cat data.out | awk '{print $2,$4}' > cai.dat
+cat data.out | awk '{print $2,$5}' > nai.dat
+cat data.out | awk '{print $2,$6}' > ina.dat
+cat data.out | awk '{print $2,$7}' > ical.dat
+cat data.out | awk '{print $2,$8}' > ito.dat
+cat data.out | awk '{print $2,$9}' > ikr.dat
+cat data.out | awk '{print $2,$10}' > iks.dat
+cat data.out | awk '{print $2,$11}' > ik1.dat
+cat data.out | awk '{print $2,$12}' > inak.dat
+cat data.out | awk '{print $2,$13}' > incx.dat
+cat data.out | awk '{print $2,$14}' > inab.dat
+cat data.out | awk '{print $2,$15}' > icab.dat
+cat data.out | awk '{print $2,$16}' > ipca.dat
+cat data.out | awk '{print $2,$17}' > isitm.dat
